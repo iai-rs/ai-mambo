153b596a186f0e0ea831fbe2f48b3c3dbce4b0cb
Bojan Baltic <bojan.baltic@gmail.com>
2024-05-08 16:20:36 +0200

    Test cicd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default async function Home() {
     <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Mambo <span className="text-[hsl(280,100%,70%)]">AI</span>
+          Mambo <span className="text-[hsl(280,100%,70%)]">vestacka inteligencija</span>
         </h1>
         <div className="flex flex-col gap-3">
           {birads.map(({ model_1_result, study_uid }) => {
