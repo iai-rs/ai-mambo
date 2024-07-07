a07f35aaa7a77bf16c0ec04b242fef8f3a3a1726
Vladimir <dabic.vladimir@gmail.com>
2024-07-07 21:28:03 +0200

    feat: details page refine cards

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -1,6 +1,6 @@
 import { ArrowLeft } from "lucide-react";
 import Link from "next/link";
-import React, { Suspense } from "react";
+import React, { ReactNode, Suspense } from "react";
 import { auth } from "~/auth";
 import PatientGallery from "~/components/PatientGallery";
 import { Button } from "~/components/ui/button";
@@ -13,6 +13,11 @@ import {
 } from "~/components/ui/card";
 import { api } from "~/trpc/server";
 import { parseDateFormat } from "~/utils/parseDateFormat";
+import { getPatientAge } from "~/utils/parseJMBG";
+
+const LeftText = ({ children }: { children: ReactNode }) => (
+  <strong className="mr-2 text-muted-foreground">{children}</strong>
+);
 
 const DetailPage = async ({ params: { id } }: { params: { id: string } }) => {
   const [patient_id = "", acquisition_date = ""] = id.split("-");
@@ -37,25 +42,48 @@ const DetailPage = async ({ params: { id } }: { params: { id: string } }) => {
             <ArrowLeft />
           </Button>
         </Link>
-        <div className="mb-4 mt-2 flex h-fit gap-2">
-          <Card>
-            <CardHeader>
-              <CardTitle>{data[0]?.patientName}</CardTitle>
-              <CardDescription>
-                {parseDateFormat(data[0]?.acquisitionDate ?? "")}
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <div className="flex">JMBG: {data[0]?.patientId}</div>
-            </CardContent>
-          </Card>
-          <Card>
-            <CardContent className="mt-4">
-              <div className="flex">Institucija: {data[0]?.institution}</div>
-              <div className="flex">Proizvođač: {data[0]?.manufacturer}</div>
-              <div className="flex">Model: {data[0]?.manufacturerModel}</div>
-            </CardContent>
-          </Card>
+        <div className="mb-4 mt-2 flex h-fit max-w-[1042px] gap-2">
+          <div className="flex flex-col">
+            <h2 className="my-4 text-2xl font-bold">{data[0]?.patientName}</h2>
+            <div className="grid  gap-2 md:grid-cols-3">
+              <Card>
+                <CardContent className="mt-4">
+                  <div className="flex">
+                    <LeftText>{"JMBG:"}</LeftText>
+                    <span>{data[0]?.patientId}</span>
+                  </div>
+                  <div>
+                    <LeftText>{"Starost pacijentkinje:"}</LeftText>
+                    <span>{getPatientAge(data[0]?.patientId ?? "")}</span>
+                  </div>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardContent className="mt-4">
+                  <div className="flex">
+                    <LeftText>{"Datum pregleda:"}</LeftText>
+                    {parseDateFormat(data[0]?.acquisitionDate ?? "")}
+                  </div>
+                  <div className="flex">
+                    <LeftText>{"Institucija:"}</LeftText>
+                    <span>{data[0]?.institution}</span>
+                  </div>
+                </CardContent>
+              </Card>
+              <Card>
+                <CardContent className="mt-4">
+                  <div className="flex">
+                    <LeftText>{"Proizvođač:"}</LeftText>
+                    <span>{data[0]?.manufacturer}</span>
+                  </div>
+                  <div className="flex">
+                    <LeftText>{"Model:"}</LeftText>
+                    <span>{data[0]?.manufacturerModel}</span>
+                  </div>
+                </CardContent>
+              </Card>
+            </div>
+          </div>
         </div>
         <PatientGallery email={email} data={data} role={role} />
       </div>
---
+++ src/utils/parseJMBG.ts
@@ -0,0 +1,42 @@
+import { differenceInYears } from "date-fns";
+
+function parsePatientDate(dateString: string) {
+  try {
+    // Extract day, month, and year parts
+    const day = parseInt(dateString.slice(0, 2), 10);
+    const month = parseInt(dateString.slice(2, 4), 10) - 1; // month is zero-based in JavaScript
+    const yearPart = parseInt(dateString.slice(4, 7), 10);
+    const yearPartLastTwo = parseInt(dateString.slice(5, 7), 10);
+
+    const year = yearPart < 100 ? 2000 + yearPartLastTwo : 1000 + yearPart;
+
+    // Create the birthdate
+    const birthdate = new Date(year, month, day);
+    console.log({ birthdate, day, month, year, yearPart });
+
+    // Check if the date is valid
+    if (isNaN(birthdate.getTime())) {
+      throw new Error("Invalid date");
+    }
+
+    return birthdate;
+  } catch (error) {
+    return null;
+  }
+}
+
+function calculateAge(birthdate: Date) {
+  return differenceInYears(new Date(), birthdate);
+}
+
+function getPatientAge(dateString: string) {
+  const birthdate = parsePatientDate(dateString);
+
+  if (!birthdate) {
+    return birthdate;
+  }
+
+  return calculateAge(birthdate);
+}
+
+export { getPatientAge };
