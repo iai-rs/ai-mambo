975dd1ff0530f0c739bb1c53860c71a7cae515ac
Vladimir <dabic.vladimir@gmail.com>
2024-12-08 13:17:01 +0100

    use worker from cdn

--- src/components/Help/index.tsx
+++ src/components/Help/index.tsx
@@ -9,10 +9,7 @@ import "react-pdf/dist/esm/Page/TextLayer.css";
 import "./pdf.css";
 import Link from "next/link";
 
-pdfjs.GlobalWorkerOptions.workerSrc = new URL(
-  "pdfjs-dist/build/pdf.worker.min.mjs",
-  import.meta.url,
-).toString();
+pdfjs.GlobalWorkerOptions.workerSrc = `//unpkg.com/pdfjs-dist@${pdfjs.version}/build/pdf.worker.min.mjs`;
 
 const options = {
   cMapUrl: "/cmaps/",
